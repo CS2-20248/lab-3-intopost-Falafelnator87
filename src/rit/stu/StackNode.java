8d7ac959fdf224473d8505e216e29ba3e7ada231
peter <peternator87@gmail.com>
2025-06-01 22:30:24 -0400

    Finished implementing StackNode.

--- src/rit/stu/StackNode.java
+++ src/rit/stu/StackNode.java
@@ -1,6 +1,9 @@
 package rit.stu;
 
+import rit.cs.Node;
 import rit.cs.Stack;
+
+import java.util.ArrayList;
 
 /**
  * A stack implementation that uses a Node to represent the structure.
@@ -9,28 +12,36 @@ import rit.cs.Stack;
  * @author RIT CS
  */
 public class StackNode<T> implements Stack<T> {
+    private Node<T> top;
 
     /**
      * Create an empty stack.
      */
     public StackNode() {
+        top = null;
     }
 
     @Override
     public boolean empty() {
-        return false;
+        return top == null;
     }
 
     @Override
     public T pop() {
-        return null;
+        assert !empty();
+        T data = top.getData();
+        top = top.getNext();
+        return data;
     }
 
     @Override
-    public void push(T element) {}
+    public void push(T element) {
+        top = new Node<>(element, top);
+    }
 
     @Override
     public T top() {
-        return null;
+        assert !empty();
+        return top.getData();
     }
 }
