2552ab939117ada7498cc4a86de4fe16bc4aeb98
peter <peternator87@gmail.com>
2025-06-01 22:46:41 -0400

    Finished implementing QueueNode.

--- src/rit/stu/QueueNode.java
+++ src/rit/stu/QueueNode.java
@@ -10,33 +10,50 @@ import rit.cs.Queue;
  * @author RIT CS
  */
 public class QueueNode<T> implements Queue<T> {
+    private Node<T> front;
+    private Node<T> back;
     /**
      * Create an empty queue.
      */
     public QueueNode() {
+        front = null;
+        back = null;
     }
 
     @Override
     public T back() {
-        return null;
+        assert !empty();
+        return back.getData();
     }
 
     @Override
     public T dequeue() {
-        return null;
+        assert !empty();
+        T data = front.getData();
+        front = front.getNext();
+        return data;
     }
 
     @Override
     public boolean empty() {
-        return false;
+        return front == null;
     }
 
     @Override
     public void enqueue(T element) {
+        Node<T> newNode = new Node<>(element, null);
+        if (empty()) {
+            front = newNode;
+            back = newNode;
+        } else {
+            back.setNext(newNode);
+            back = back.getNext();
+        }
     }
 
     @Override
     public T front() {
-        return null;
+        assert !empty();
+        return front.getData();
     }
 }
